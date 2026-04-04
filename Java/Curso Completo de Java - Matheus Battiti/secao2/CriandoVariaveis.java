a9cda3f86859ceb9b8428997bda99182c09f309b
Joao Victor R <joaovicctorribeirobatista@gmail.com>
2026-04-04 08:21:59 -0300

    Delete Java/Curso Completo de Java - Matheus Battiti directory

--- Java/Curso Completo de Java - Matheus Battiti/secao2/CriandoVariaveis.java
+++
@@ -1,8 +0,0 @@
-public class CriandoVariaveis {
-        
-    int idade = 25;
-    String nome = "João";
-    boolean isEstudante = true;
-    double altura = 1.75;
-    
-}
--- Java/Curso Completo de Java - Matheus Battiti/secao8/Secao8_Exercicio3.java
+++
@@ -1,9 +0,0 @@
-public class Secao8_Exercicio3 {
-
-    public static void main(String[] args) {
-        
-
-    }
-
-    // Funções
-}
